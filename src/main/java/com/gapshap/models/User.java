694e37bbff88d4510a4578aabe3ce93e098f1602
umesh <bavisakrumeshusb97@gmail.com>
2025-08-08 09:34:21 +0530

    updated code

--- src/main/java/com/gapshap/models/User.java
+++ src/main/java/com/gapshap/models/User.java
@@ -9,6 +9,68 @@ public class User {
 	private String address;
 	private String eduction;
 	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(int id, String name, String email, String address, String eduction) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.address = address;
+		this.eduction = eduction;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEduction() {
+		return eduction;
+	}
+
+	public void setEduction(String eduction) {
+		this.eduction = eduction;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", address=" + address + ", eduction="
+				+ eduction + "]";
+	}
+	
+	
+	
 	
 
 }
